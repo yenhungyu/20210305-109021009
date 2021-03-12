5ba91fbeecab053188930bf154744e26020c315f
unknown <henry13458@gmail.com>
2021-03-12 14:59:54 +0800

    update week3_classwork.py commit

--- week3_classwork.py
+++ week3_classwork.py
@@ -1,7 +1,7 @@
-class student:
+class Student:
     def __init__(self ,name ,id):
         self.studName =name
         self.studId =id
 
-x =student("卡卡" , "109021009")
+x =Student("卡卡" , "109021009")
 print(x.studName, "\t", x.studId)
--- work.py
+++ work.py
@@ -2,8 +2,8 @@ import sys
 import Mymodule as my
 
 if sys.argv[1] == "-f":
-    val=int(sys.argv[2])
+    val =int(sys.argv[2])
     print(my.getFibonacci(val))
 elif sys.argv[1] == "-n":
-    val = int(sys.argv[2])
+    val =int(sys.argv[2])
     print(my.getNumberDigits(val))
