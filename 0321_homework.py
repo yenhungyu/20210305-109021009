da7e9ff26d27f6ed44218f5bf9ad74c3863b4ade
yenhungyu <80086989+yenhungyu@users.noreply.github.com>
2021-06-20 16:16:14 +0800

    Delete 0321_homework.py

--- 0321_homework.py
+++
@@ -1,9 +0,0 @@
-class Student:
-    def __init__ (self , name , id   ):
-        self.studname=name
-        self.studid= Id
-
-x1 =Student("卡卡" , "109021009")
-x2 =Student("卡卡" , "109021009")
-x3 =Student("卡卡" , "109021009")
-print(x.studName, "\t", x.studId)
